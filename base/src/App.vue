8df467035783cbac3cf0dc9c57a7fdf65c549110
Sciederrick <derrickmbarani@gmail.com>
2020-05-31 19:22:00 +0300

    project skeleton

---
+++ base/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <nav>
+      <h1>portability</h1>
+    </nav>
+    <landingPage></landingPage>
+  </div>
+</template>
+
+<script>
+import landingPage from './components/LandingPage.vue'
+
+export default {
+  components:{
+    'landingPage':landingPage
+  },
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+:root {
+  font-family: "Courier New", monospace;
+}
+nav {
+  font-variant: small-caps;
+  text-align: center;
+}
+</style>

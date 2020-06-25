e4b7d05c2505b718e48de26a1d1e8606a4ea6c05
d3vkk <donaldkk22@yahoo.com>
2020-06-25 18:55:51 +0300

    get current location of client
    Gets the current location of the client using the HTML5 geolocation API

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -40,10 +40,33 @@ export default {
         .catch((err)=>{
           console.log(err);
         });
+    },
+    getCurrentLocation() {
+      var options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+      };
+
+      function success(pos) {
+        var crd = pos.coords;
+
+        console.log("Your current position is:");
+        console.log(`Latitude : ${crd.latitude}`);
+        console.log(`Longitude: ${crd.longitude}`);
+        console.log(`More or less ${crd.accuracy} meters.`);
+      }
+
+      function error(err) {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+      }
+
+      navigator.geolocation.getCurrentPosition(success, error, options);
     }
   },
   beforeMount() {
     this.getKey();
+    this.getCurrentLocation();
   }
 };
 </script>
